4fd130b7c62740fa53e41e1912e489f39b742082
unknown <kondr2004k@gmail.com>
2025-08-19 22:43:24 +0300

    created logic for cart. Added CART_SESSION_ID in settings

---
+++ shop/cart/cart.py
@@ -0,0 +1,57 @@
+from django.conf import settings
+from ..main.models import Product
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID) # тут круглые
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {} # тут квадратные
+
+        self.cart = cart
+
+    # quantity указывает какое будет значение по умолчанию для количества добавляемых товаров в корзину.
+    # override_quantity - для большого кол-ва.
+    def add(self, product, quantity=1, override_quantity=False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': product.price}
+
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def save(self):
+        self.session.modified = True
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = float(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(float(item['price'] * item['quantity']) for item in self.cart.values())
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
